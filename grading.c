e7ea482b39c531a1d4f17759597e5ba42638cfd0
Seyed Mahmoud Shahrokni <mahmoud.shahrokni@celus.io>
2024-11-12 10:40:31 +0100

    grading

---
+++ grading.c
@@ -0,0 +1,24 @@
+/* https://www.hackerrank.com/challenges/grading */
+/* Difficulty Easy */
+#include <stdlib.h>
+
+int *gradingStudents(int grades_count, int *grades, int *result_count) {
+
+  int *result = malloc(sizeof(int) * grades_count);
+
+  for (int i = 0; i < grades_count; i += 1) {
+    if (grades[i] < 38)
+      result[i] = grades[i];
+    else {
+      int next_mul_five = grades[i] + abs(5 - (grades[i] % 5));
+      if (next_mul_five - grades[i] < 3)
+        result[i] = next_mul_five;
+      else
+        result[i] = grades[i];
+    }
+  }
+
+  *result_count = grades_count;
+
+  return result;
+}
